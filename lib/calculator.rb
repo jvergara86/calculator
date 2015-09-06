45dbdf5884a147a43e6769f9c78d8ac71b3a9d04
Joseph E Vergara <jvergara3086@gmail.com>
2015-09-06 15:14:06 -0400

    After subtraction tests have been created.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -4,4 +4,15 @@ class Calculator
     numbers.inject(:+)
   end
   
+  def subtract *numbers
+    numbers.inject(:-)
+  end
+  
+  def multiply *numbers
+    numbers.inject(:*)
+  end
+  
+  def divide *numbers
+    numbers.inject(:/)
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -24,4 +24,30 @@ describe Calculator do
     end
   end
   
+  describe "#subtract" do
+    it "returns an integer" do
+      expect(calc.subtract 1, 1).to be_an Integer
+    end
+	
+   it "is dependent on the operation order" do
+      expect(calc.subtract 1, 2).not_to eq calc.add(2, 1)
+    end
+
+    context "when subtracting zero from an integer" do
+      it "returns the integer" do
+        expect(calc.subtract 4, 0).to eq 4
+      end
+    end
+	
+	context "when subtracting an integer from itself" do
+		it "returns zero" do
+			expect(calc.subtract 3,3).to eq 0
+		end
+	end
+ 
+    it "accepts more than 2 arguments" do
+      expect(calc.subtract 2, 4, 3).to eq -5
+    end
+  end
+  
 end
